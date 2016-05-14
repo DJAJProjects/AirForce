c7120d78fa4eb68a82e56014386c3ec03549016d
kubawieczorek <jwieczorek94@wp.pl>
2016-05-14 16:08:52 +0200

    Światła statku
    Światła statku

---
+++ AirForce/Assets/Scripts/LightsController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightsController : MonoBehaviour {
+
+    private bool turnedOn;
+
+    private Light lights = null;
+
+	// Use this for initialization
+	void Start () {
+        turnedOn = false;
+        lights = this.GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (turnedOn)
+            {
+                turnedOn = false;
+                lights.enabled = false;
+            }
+            else
+            {
+                turnedOn = true;
+                lights.enabled = true;
+            }
+        }
+	}
+}
