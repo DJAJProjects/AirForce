94c81c2a789d54f125c748b8937221639aed8d51
Dominika94 <dominikablasiak2@gmail.com>
2016-06-22 02:03:06 +0200

    DOdane do sceny, next level

---
+++ AirForce/Assets/Scripts/NextLevelController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextLevelController : MonoBehaviour
+{
+
+    public static readonly string nextLevelName = "Scene01";
+    //TODO dodać efekt czząsteczkowy
+    //public GameObject explosion;
+    public GameObject nextLevelText;
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Extra");
+
+       // explosion.SetActive(true);
+        nextLevelText.SetActive(true);
+        Invoke("LoadLevel", 0.5f);
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
