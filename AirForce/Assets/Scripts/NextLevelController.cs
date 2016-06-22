f964bdd93657f52f1ea433dc5398ff28696f7ebd
kubawieczorek <jwieczorek94@wp.pl>
2016-06-22 07:24:05 +0200

    aktualizacja
    aktualizacja

--- AirForce/Assets/Scripts/NextLevelController.cs
+++ AirForce/Assets/Scripts/NextLevelController.cs
@@ -6,20 +6,34 @@ public class NextLevelController : MonoBehaviour
 {
 
     public static readonly string nextLevelName = "Scene01";
+
+    void Start()
+    {
+        nextLevelText.SetActive(false);
+    }
     //TODO dodać efekt czząsteczkowy
     //public GameObject explosion;
     public GameObject nextLevelText;
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Extra");
+        if(other.tag == "Player")
+        {
+            Debug.Log("Extra");
+            nextLevelText.SetActive(true);
+            Invoke("LoadLevel", 0.5f);
+            //Application.LoadLevel(nextLevelName);
+        }
+        
 
        // explosion.SetActive(true);
-        nextLevelText.SetActive(true);
-        Invoke("LoadLevel", 0.5f);
+       
+        //Invoke("LoadLevel", 0.5f);
     }
 
     void LoadLevel()
     {
+        //nextLevelText.SetActive(true);
         SceneManager.LoadScene(nextLevelName);
     }
 }
+    
